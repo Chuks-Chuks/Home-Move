9bd144fbee06d1db9b52346c228bef463fad87c9
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-05-02 23:03:35 +0100

    Finalised converting the csv files to parquet for faster processing.

--- scripts/process_raw_to_parquet.py
+++ scripts/process_raw_to_parquet.py
@@ -2,17 +2,25 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pathlib import Path
 
+# Creating a spark session
 spark = SparkSession.builder.appName('HomeMove Raw to Bronze').getOrCreate()
 
-RAW_PATH = '../data/raw'
-BRONZE_PATH = '../data/bronze'
+RAW_PATH = './data/raw'
+BRONZE_PATH = './data/bronze'
 
 Path(BRONZE_PATH).mkdir(parents=True, exist_ok=True) 
 
 # Reading CSVs
-customer_df = spark.read.option('header', 'true').csv(f"{RAW_PATH}/customers.csv")
-properties_df = spark.read.option('header', 'true').csv(f"{RAW_PATH}/properties.csv")
-transactions_df = spark.read.option('header', 'true').csv(f"{RAW_PATH}/transactions.csv")
-csat_df = spark.read.option('header', 'true').csv(f"{RAW_PATH}/csat_surveys.csv")
+customer_df = spark.read.option('header', 'true').csv(f"{RAW_PATH}/customers.csv", inferSchema=True)
+properties_df = spark.read.option('header', 'true').csv(f"{RAW_PATH}/properties.csv", inferSchema=True)
+transactions_df = spark.read.option('header', 'true').csv(f"{RAW_PATH}/transactions.csv", inferSchema=True)
+csat_df = spark.read.option('header', 'true').csv(f"{RAW_PATH}/csat_surveys.csv", inferSchema=True)
 
-customer_df.show(5)
+# COnverting the CSV files to parquet to enable faster processing
+
+customer_df.write.mode('overwrite').parquet(f'{BRONZE_PATH}/customers')
+properties_df.write.mode('overwrite').parquet(f'{BRONZE_PATH}/properties')
+transactions_df.write.mode('overwrite').parquet(f'{BRONZE_PATH}/transactions')
+csat_df.write.mode('overwrite').parquet(f'{BRONZE_PATH}/csat')
+
+spark.stop()
