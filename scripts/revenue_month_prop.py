654ae5524424b3640626aa7c3f1ec229d86b5dd2
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-05-07 13:29:11 +0100

    Computed the monthly revenue by properties

---
+++ scripts/revenue_month_prop.py
@@ -0,0 +1,24 @@
+from constant_class import Constants
+
+const = Constants()
+
+
+"""
+Computing the revenue summary monthly by property_type
+"""
+spark = const.start_spark(title_of_app='Montly revenue for Properties sold')
+transaction_summary = spark.read.parquet(f'{const.SILVER_PATH}/transaction_summary')
+
+revenue_summary_monthly_prop = transaction_summary.filter(const.psq.col('status') == 'Completed') \
+.groupBy('property_type', 'start_month') \
+.agg(
+    const.psq.count('*').alias('num_sales'),
+    const.psq.sum('valuation').alias('total_revenue'),
+    const.psq.avg('valuation').alias('avg_revenue')
+) \
+.orderBy('start_month')
+
+# revenue_summary_monthly_prop.limit(2).show()
+
+# Writing to the gold table
+revenue_summary_monthly_prop.write.mode('overwrite').parquet(f'{GOLD_PATH}/revenue_summary_monthly_prop')
