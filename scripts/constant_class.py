6ad0037d944070de4c13f5a02bcb9d1a194e983e
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-05-08 09:36:09 +0100

    Added additional constants to allow for modularisation of the ingestion source

--- scripts/constant_class.py
+++ scripts/constant_class.py
@@ -13,6 +13,12 @@ class Constants:
         self.RAW_PATH = './data/raw'
         self.ABSOLUTE_PATH = "./Users/phili/etl_project/homemove-analytics"
         self.psq = psq
+        self.NUM_CUSTOMERS = 5000
+        self.NUM_PROPERTIES = 3000
+        self.NUM_TRANSACTIONS = 4500
+        self.NUM_CSATS = 1500
+        self.MINIMUM_HOUSE_PRICE = 150000
+        self.MAXIMUM_HOUSE_PRICE = 750000
 
     
     def start_spark(self, title_of_app: str) -> SparkSession: 
