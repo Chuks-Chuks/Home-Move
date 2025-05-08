01cc2a72251444a29fcba618c2dfa666eaec4c3c
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-05-08 15:27:18 +0100

    Finalised the ingestion logic with Airflow.

--- orchestration/airflow/dags/homemove_dag.py
+++ orchestration/airflow/dags/homemove_dag.py
@@ -1,12 +1,11 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-from constant_class import Constants
-from generate_fake_data import IngestData
+from scripts import constant_class as c
+from scripts import generate_fake_data as gfd
 
-const = Constants()
-ingestion = IngestData()
+const = c.Constants()
+ingestion = gfd.IngestData()
 
 def fetch_customers():
     return ingestion.fetch_customers()
@@ -34,9 +33,33 @@ with DAG(
     schedule_interval='@daily',
     catchup=False
 ) as dag:
-    
     # Fetch the raw data 
-    get_raw_data = PythonOperator(
-        'task_id' = 'fetch_data',
-        'bash_command' = f'spark-submit {const.ABSOLUTE_PATH}/scripts/generate_fake_data.py'
+
+    # Fetch the customers
+    task_get_customers = PythonOperator(
+        task_id='fetch_customers',
+        python_callable=fetch_customers,
+        dag=dag
     )
+
+    # Fetch the properties
+    task_get_properties = PythonOperator(
+        task_id='fetch_properties',
+        python_callable=fetch_properties,
+        dag=dag
+    )
+
+    # Fetch the transactions
+    task_fetch_transactions = PythonOperator(
+        task_id='fetch_transactions',
+        python_callable=fetch_transactions,
+        dag=dag
+    )
+
+    # Fetch the customer satisfaction
+    task_fetch_cust_feedback = PythonOperator(
+        task_id='fetch_cust_feedback',
+        python_callable=fetch_cust_feedback,
+        dag=dag
+    )
+
