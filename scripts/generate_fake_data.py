ec4f18b3cdc079f9d28e3f2d7bb047e8a31cca4e
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-05-09 13:09:30 +0100

    Implemented logging into the class to track failures

--- scripts/generate_fake_data.py
+++ scripts/generate_fake_data.py
@@ -2,9 +2,9 @@ from faker import Faker
 import pandas as pd
 from random import seed, choice, uniform, randint
 import uuid
-from datetime import timedelta
 from pathlib import Path
 from constant_class import Constants
+import logging
 
 class IngestData(Constants):
     def __init__(self):
@@ -19,6 +19,7 @@ class IngestData(Constants):
         self.OUTPUT_DIR.mkdir(parents=True, exist_ok=True)  
         self.transactions = []
         self.transactions_ids = []
+        self.logger = logging.getLogger(__name__)
 
 # Create the directory if it doesn't already exist. 
      # 1. Customers
@@ -29,21 +30,25 @@ class IngestData(Constants):
         :returns:
             None (Creates a file of the customer's details in the raw folder)
         """
+        try:
+            for customer in range(self.NUM_CUSTOMERS):
+                self.customers.append(
+                    {
+                        'customer_id': str(uuid.uuid4())[:8],
+                        'full_name': self.fake.name(),
+                        'email': self.fake.email(),
+                        'signup_date': self.fake.date_between(start_date='-3y', end_date='-1d'),
+                        'region': choice(["North West", "London", "South East", "Scotland", "East Midlands", "West Midlands", "South West", "Yorkshire and the Humber"]),
+                        'source_channel': choice(["Website", "Referral", "Estate Agent"])
+                    }
+                )  # Pulling mock data to fit the customer schema
 
-        for customer in range(self.NUM_CUSTOMERS):
-            self.customers.append(
-                {
-                    'customer_id': str(uuid.uuid4())[:8],
-                    'full_name': self.fake.name(),
-                    'email': self.fake.email(),
-                    'signup_date': self.fake.date_between(start_date='-3y', end_date='-1d'),
-                    'region': choice(["North West", "London", "South East", "Scotland", "East Midlands", "West Midlands", "South West", "Yorkshire and the Humber"]),
-                    'source_channel': choice(["Website", "Referral", "Estate Agent"])
-                }
-            )  # Pulling mock data to fit the customer schema
-
-        df_customers = pd.DataFrame(self.customers)
-        return df_customers.to_csv(self.OUTPUT_DIR / "customers.csv", index=False)
+            df_customers = pd.DataFrame(self.customers)
+            self.logger.info("The customers' details have been saved successfully")
+            return df_customers.to_csv(self.OUTPUT_DIR / "customers.csv", index=False)
+        except Exception as e:
+            self.logger.error(f"An error has occurred and the customers' upload has failed:{str(e)}")
+            raise
 
     # 2. Properties = []
     def fetch_properties(self) -> None:
@@ -53,21 +58,25 @@ class IngestData(Constants):
         :returns:
             None (Writes csv file to raw folder)
         """
-        for _ in range(self.NUM_PROPERTIES):
-            self.properties.append(
-                {
-                    'property_id': str(uuid.uuid4())[:5],
-                    'address': self.fake.street_address(),
-                    'postcode': self.fake.postcode(),
-                    'property_type': choice(["Flat", "Detached", "Terraced", "Semi-detached"]),
-                    'valuation': round(uniform(self.MINIMUM_HOUSE_PRICE, self.MAXIMUM_HOUSE_PRICE), 2),
-                    'region': choice(["North West", "London", "South East", "Scotland", "East Midlands", "West Midlands", "South West", "Yorkshire and the Humber"])
-                }
-            )
+        try:
+            for _ in range(self.NUM_PROPERTIES):
+                self.properties.append(
+                    {
+                        'property_id': str(uuid.uuid4())[:5],
+                        'address': self.fake.street_address(),
+                        'postcode': self.fake.postcode(),
+                        'property_type': choice(["Flat", "Detached", "Terraced", "Semi-detached"]),
+                        'valuation': round(uniform(self.MINIMUM_HOUSE_PRICE, self.MAXIMUM_HOUSE_PRICE), 2),
+                        'region': choice(["North West", "London", "South East", "Scotland", "East Midlands", "West Midlands", "South West", "Yorkshire and the Humber"])
+                    }
+                )
 
-        df_properties = pd.DataFrame(self.properties)
-        return df_properties.to_csv(self.OUTPUT_DIR / "properties.csv", index=False)
-
+            df_properties = pd.DataFrame(self.properties)
+            self.logger.info("Properties fetched successfuy")
+            return df_properties.to_csv(self.OUTPUT_DIR / "properties.csv", index=False)
+        except Exception as e:
+            self.logger.error(f"Failed to fetch properties: {str(e)}")
+            raise
 
     # 3. Transactions
     def fetch_transactions(self) -> None:
@@ -77,31 +86,35 @@ class IngestData(Constants):
         :returns:   
             None (Writes transaction details to the raw folder)
         """
+        try:
+            for transaction in range(self.NUM_TRANSACTIONS):
+                customer = choice(self.customers)
+                property_choice = choice(self.properties)
+                start_date = self.fake.date_between(start_date='-2y', end_date='-20d')
+                status = choice(["Started", "Under Offer", "Completed", "Abandoned"])
+                complete_date = self.fake.date_between(start_date=start_date, end_date='today') if status == "Completed" else "N/A"
+                transactions_id = str(uuid.uuid4())
+                self.transactions_ids.append(transactions_id)
 
-        for transaction in range(self.NUM_TRANSACTIONS):
-            customer = choice(self.customers)
-            property_choice = choice(self.properties)
-            start_date = self.fake.date_between(start_date='-2y', end_date='-20d')
-            status = choice(["Started", "Under Offer", "Completed", "Abandoned"])
-            complete_date = self.fake.date_between(start_date=start_date, end_date='today') if status == "Completed" else "N/A"
-            transactions_id = str(uuid.uuid4())
-            self.transactions_ids.append(transactions_id)
+                self.transactions.append(
+                    {
+                        'transaction_id': transactions_id,
+                        'customer_id': customer['customer_id'],
+                        'property_id': property_choice['property_id'],
+                        'solicitor_name': self.fake.name(),
+                        'status': status,
+                        'start_date': start_date,
+                        'completion_date': complete_date, 
+                        'current_stage': choice(['Search', 'Contracts', 'Survey', 'Offer', 'N/A']) if status != 'Completed' else "N/A"
+                    }
+                )
 
-            self.transactions.append(
-                {
-                    'transaction_id': transactions_id,
-                    'customer_id': customer['customer_id'],
-                    'property_id': property_choice['property_id'],
-                    'solicitor_name': self.fake.name(),
-                    'status': status,
-                    'start_date': start_date,
-                    'completion_date': complete_date, 
-                    'current_stage': choice(['Search', 'Contracts', 'Survey', 'Offer', 'N/A']) if status != 'Completed' else "N/A"
-                }
-            )
+            df_transactions = pd.DataFrame(self.transactions)
+            self.logger.info("Transactions fetched successfuy")
+            return df_transactions.to_csv(self.OUTPUT_DIR / "transactions.csv", index=False)
+        except Exception as e:
+            self.logger.error(f"Failed to fetch transactions {str(e)}")
 
-        df_transactions = pd.DataFrame(self.transactions)
-        return df_transactions.to_csv(self.OUTPUT_DIR / "transactions.csv", index=False)
 
 
     # 4. CSAT (Customer Satisfaction)
@@ -112,18 +125,22 @@ class IngestData(Constants):
             :returns:
                 None (Writes csv file to raw folder)
         """
-        for review in range(self.NUM_CSATS):
-            transaction = choice(self.transactions)
-            if transaction['status'] != 'Abadoned':
-                self.csat.append(
-                    {
-                        'survey_id': str(uuid.uuid4())[:6],
-                        'transaction_id': transaction['transaction_id'],
-                        'survey_date': self.fake.date_between(start_date=transaction['start_date'], end_date='today'),
-                        'score': randint(1, 5),
-                        'comment': self.fake.sentence(nb_words=15)
-                    }
-                )
+        try:
+            for review in range(self.NUM_CSATS):
+                transaction = choice(self.transactions)
+                if transaction['status'] != 'Abadoned':
+                    self.csat.append(
+                        {
+                            'survey_id': str(uuid.uuid4())[:6],
+                            'transaction_id': transaction['transaction_id'],
+                            'survey_date': self.fake.date_between(start_date=transaction['start_date'], end_date='today'),
+                            'score': randint(1, 5),
+                            'comment': self.fake.sentence(nb_words=15)
+                        }
+                    )
 
-        df_csat = pd.DataFrame(self.csat)
-        return df_csat.to_csv(self.OUTPUT_DIR / "csat_surveys.csv", index=False)
+            df_csat = pd.DataFrame(self.csat)
+            self.logger.info("Customer feedback fetched successfully")
+            return df_csat.to_csv(self.OUTPUT_DIR / "csat_surveys.csv", index=False)
+        except Exception as e:
+            self.logger.error(f"The customer feedback data has failed to load: {str(e)}")
