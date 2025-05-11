1d206050e4e56feba1a12d360599f9aa395f2427
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-05-11 10:42:10 +0100

    Ensured the appropraite namijg convention and styling was used to stop errors

--- orchestration/airflow/dags/bronze_pipeline_dag.py
+++ orchestration/airflow/dags/bronze_pipeline_dag.py
@@ -2,14 +2,21 @@ import logging
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
-from scripts import constant_class as c
-
 # Setup logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+try:
+    from scripts.constant_class import Constants
+except ImportError as ie:
+    logging.warning("Import failed! Now trying alternative solution")
+    import sys
+    sys.path.insert(0, '/opt/airflow')
+    from scripts.constant_class import Constants
 
-const = c.Constants()
+
+
+const = Constants()
 
 default_args = {
     'owner': 'chuks-chuks', 
@@ -18,16 +25,16 @@ default_args = {
 }
 
 with DAG(
-    dag_id='bronze layer pipeline',
+    dag_id='bronze_layer_pipeline',
     default_args=default_args,
     description='convert raw cvs to parquet files using PySpark',
     start_date=datetime(2024, 1, 1),
-    schudule_interval='@daily',
+    schedule_interval='@daily',
     catchup=False
 ) as dag:
     raw_to_bronze = BashOperator(
-        task_id='Transform CSVs to parquet files',
-        bash_command='spark-submit /Users/phili/etl_project/homemove-analytics/scripts/process_raw_to_parquet.py'
+        task_id='transform-csvs-to-parquet-files',
+        bash_command='spark-submit /Users/phili/etl_project/homemove-analytics/orchestration/airflow/scripts/process_raw_to_parquet.py'
     )
 
 logging.info("DAG initialised and transformation to bronze completed")
