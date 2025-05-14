00e8a839671db5d08fd3274f2e7e10549fc2566f
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-05-14 13:10:21 +0100

    Ensure correct path discovery

--- orchestration/airflow/scripts/process_raw_to_parquet.py
+++ orchestration/airflow/scripts/process_raw_to_parquet.py
@@ -17,7 +17,7 @@ Path(BRONZE_PATH).mkdir(parents=True, exist_ok=True)
 customer_df = spark.read.option('header', 'true').csv(f"{const.RAW_PATH}/customers.csv", inferSchema=True)
 properties_df = spark.read.option('header', 'true').csv(f"{const.RAW_PATH}/properties.csv", inferSchema=True)
 transactions_df = spark.read.option('header', 'true').csv(f"{const.RAW_PATH}/transactions.csv", inferSchema=True)
-csat_df = spark.read.option('header', 'true').csv(f"{const.RAW_PATH}/csat_surveys.csv", inferSchema=True)
+csat_df = spark.read.option('header', 'true').csv(f"{const.RAW_PATH}/csat.csv", inferSchema=True)
 
 # COnverting the CSV files to parquet to enable faster processing
 try:
