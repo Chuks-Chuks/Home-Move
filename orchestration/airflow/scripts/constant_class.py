a77a42aac990cdfd4cdd674ae1a114637128698f
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-05-14 13:03:47 +0100

    Esnure the paths pointed to the mounted volumes in Docker.

--- orchestration/airflow/scripts/constant_class.py
+++ orchestration/airflow/scripts/constant_class.py
@@ -3,11 +3,12 @@ import pyspark.sql.functions as psq
 
 class Constants:
     def __init__(self):
-        self.BRONZE_PATH = './data/bronze'
-        self.SILVER_PATH = './data/silver'       
-        self.GOLD_PATH = './data/gold'
-        self.REPORTING_PATH = './data/reporting'
-        self.RAW_PATH = './data/raw'
+        self.BASE_PATH = '/opt/airflow/'
+        self.BRONZE_PATH = f'{self.BASE_PATH}/data/bronze'
+        self.SILVER_PATH = f'{self.BASE_PATH}/data/silver'       
+        self.GOLD_PATH = f'{self.BASE_PATH}/data/gold'
+        self.REPORTING_PATH = f'{self.BASE_PATH}/data/reporting'
+        self.RAW_PATH = f'{self.BASE_PATH}/data/raw'
         self.ABSOLUTE_PATH = "./Users/phili/etl_project/homemove-analytics"
         self.psq = psq
         self.NUM_CUSTOMERS = 15000
